88ca432acae15e3a1591c820e8882002eca9fd0b
scott coulton <scottc@autopilothq.com>
2017-04-18 09:26:01 -0500

    added various things

--- modules/ucpconfig/manifests/compose.pp
+++ modules/ucpconfig/manifests/compose.pp
@@ -1,13 +1,13 @@
 class ucpconfig::compose {
 
-  file { ['/etc/interlock', '/etc/jenkins']:
+  file { ['/etc/interlock', '/etc/jenkins', '/etc/elk']:
     ensure => directory,
     }
 
   file { '/etc/interlock/docker-compose.yml':
     ensure  => file,
     content => template('ucpconfig/interlock.yml.erb'),
-    require => File['/etc/interlock', '/etc/jenkins'],
+    require => File['/etc/interlock', '/etc/jenkins', '/etc/elk'],
     }
 
   exec { 'docker-compose-interlock':
@@ -21,8 +21,9 @@ class ucpconfig::compose {
   file { '/etc/jenkins/docker-compose.yml':
     ensure  => file,
     content => template('ucpconfig/jenkins.yml.erb'),
-    require => File['/etc/interlock', '/etc/jenkins'],
+    require => File['/etc/interlock', '/etc/jenkins', '/etc/elk'],
     }
+
 
   exec { 'docker-compose-jenkins':
     command => 'bash -l -c "docker-compose -f /etc/jenkins/docker-compose.yml up -d"',
@@ -30,4 +31,25 @@ class ucpconfig::compose {
     timeout => '1800',
     require => [ Class['ucpconfig::config'], Exec['docker-compose-interlock']]
     }
+ 
+ exec { 'dtr':
+    command => 'bash -l -c "docker run -t --rm docker/dtr install --dtr-external-url https://172.17.10.103 --ucp-node ucp-03 --ucp-username admin --ucp-password orca4307  --ucp-insecure-tls --ucp-url https://172.17.10.101"',
+    path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+    timeout => '3600',
+    require => [ Class['ucpconfig::config'], Exec['docker-compose-interlock']]
+    }
+
+  vcsrepo {'/etc/dockercon/':
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/jzaccone/office-space-dockercon2017.git',
+   }->
+
+   exec { 'docker-compose-dockercon':
+    command => 'bash -l -c "docker-compose -f /etc/dockercon/docker-compose.yml up -d"',
+    path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+    timeout => '3600',
+    require => [ Class['ucpconfig::config'], Exec['docker-compose-interlock']]
+    }  
+
 }
