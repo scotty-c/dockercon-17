7bcceb6e22bf5ef9aed1d828c7e20453fce71f3b
scott coulton <scottc@autopilothq.com>
2017-05-11 08:41:12 +1000

    changed to use updated ucp module from GitHub

--- modules/ucpconfig/manifests/compose.pp
+++ modules/ucpconfig/manifests/compose.pp
@@ -26,30 +26,36 @@ class ucpconfig::compose {
 
 
   exec { 'docker-compose-jenkins':
-    command => 'bash -l -c "docker-compose -f /etc/jenkins/docker-compose.yml up -d"',
+    command => 'docker-compose -f /etc/jenkins/docker-compose.yml up -d',
+    environment => 'HOME=/root',
     path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-    timeout => '1800',
     require => [ Class['ucpconfig::config'], Exec['docker-compose-interlock']]
     }
  
- exec { 'dtr':
-    command => 'bash -l -c "docker run -t --rm docker/dtr install --dtr-external-url https://172.17.10.103 --ucp-node ucp-03 --ucp-username admin --ucp-password orca4307  --ucp-insecure-tls --ucp-url https://172.17.10.101"',
-    path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-    timeout => '3600',
-    require => [ Class['ucpconfig::config'], Exec['docker-compose-interlock']]
-    }
-
-  vcsrepo {'/etc/dockercon/':
+  docker_ucp::dtr {'Dtr install':
+    install => true,
+    dtr_version => 'latest',
+    dtr_external_url => 'https://172.17.10.104',
+    ucp_node => 'ucp-04',
+    ucp_username => 'admin',
+    ucp_password => 'orca4307',
+    ucp_insecure_tls => true,
+    dtr_ucp_url => 'https://172.17.10.101',
+    require => [ Class['ucpconfig::config'], Exec['docker-compose-interlock']] 
+    } ->
+   
+  
+   vcsrepo {'/etc/dockercon/':
     ensure   => present,
     provider => git,
     source   => 'https://github.com/jzaccone/office-space-dockercon2017.git',
    }->
 
    exec { 'docker-compose-dockercon':
-    command => 'bash -l -c "docker-compose -f /etc/dockercon/docker-compose.yml up -d"',
+    command => 'docker-compose -f /etc/dockercon/docker-compose.yml up -d',
     path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-    timeout => '3600',
+    environment => 'HOME=/root',
+    timeout => 0,
     require => [ Class['ucpconfig::config'], Exec['docker-compose-interlock']]
     }  
-
 }
